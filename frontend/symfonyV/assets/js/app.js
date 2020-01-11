b871d0cf2248faf7c1fec1ded78fa8b234f895cc
Jacques Artgraven <jacques@bidvestalice.com>
2020-01-11 13:47:48 +0200

    vue bootstrap

--- frontend/symfonyV/assets/js/app.js
+++ frontend/symfonyV/assets/js/app.js
@@ -6,10 +6,20 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-//import '../css/app.css';
+
 import Vue from 'vue';
 import App from './components/App.vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+    // Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import '../css/app.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
--- frontend/symfonyV/assets/js/components/App.vue
+++ frontend/symfonyV/assets/js/components/App.vue
@@ -1,9 +1,14 @@
 <template>
-<h2>we got the vue if you have the symfony</h2>
+  <b-container class="bv-example-row">
+    <h2>we got the vue if you have the symfony in test</h2>
+    <b-row>
+      <b-col>1 of 3</b-col>
+      <b-col>2 of 3</b-col>
+      <b-col>3 of 3</b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
