56359a76e030e163a97278bd7eb822f614355b58
Jacques Artgraven <jacques@bidvestalice.com>
2020-01-13 17:15:59 +0200

    basic tables

--- frontend/symfonyV/assets/js/components/App.vue
+++ frontend/symfonyV/assets/js/components/App.vue
@@ -65,24 +65,15 @@
 
             <!-- Right aligned nav items -->
             <b-navbar-nav class="ml-auto">
-              <b-nav-form>
-                <b-form-input
-                  size="sm"
-                  class="mr-sm-2"
-                  placeholder="Search"
-                ></b-form-input>
-                <b-button size="sm" class="my-2 my-sm-0" type="submit"
-                  >Search</b-button
-                >
-              </b-nav-form>
+        
 
               <b-nav-item-dropdown right>
                 <!-- Using 'button-content' slot -->
                 <template v-slot:button-content>
                   <em>User</em>
                 </template>
-                <b-dropdown-item href="#">Profile</b-dropdown-item>
-                <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+         
+                <b-dropdown-item  v-on:click='login = !login'>Sign Out</b-dropdown-item>
               </b-nav-item-dropdown>
             </b-navbar-nav>
           </b-collapse>
@@ -182,10 +173,7 @@ export default {
         url: url,
       })
         .then((data)=>{
-          
-          data.data.forEach(el => {
-            this.suppliers_content.push({ CompanyName: e.CompanyName, ContactName: e.ContactName, ContactTitle: e.ContactTitle })
-          });
+          this.suppliers_content = data.data
         })
         .catch(err => console.log(err));
     },
